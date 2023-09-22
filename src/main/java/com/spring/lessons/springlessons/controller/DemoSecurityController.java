d4d95ea6b0990d5ba9da548205a775dcd3d53dc3
sdjermouni <soufiane.djermouni@gmail.com>
2023-09-22 09:20:21 +0200

    Exemple Authenticate Users With Sessions avec spring Security

---
+++ src/main/java/com/spring/lessons/springlessons/controller/DemoSecurityController.java
@@ -0,0 +1,42 @@
+package com.spring.lessons.springlessons.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoSecurityController {
+
+    @GetMapping("/")
+    public String home() {
+        return
+                "<html>\n" +
+                        "  <head>\n" +
+                        "    <title>Home</title>\n" +
+                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
+                        "  </head>\n" +
+                        "  <body>\n" +
+                        "    <p>\n" +
+                        "      <a href=\"http://localhost:8080/users\">Users</a>\n" +
+                        "    </p>\n" +
+                        "    <p>\n" +
+                        "      <a href=\"http://localhost:8080/admins\">Admins</a>\n" +
+                        "    </p>\n" +
+                        "    <p>\n" +
+                        "      <a href=\"http://localhost:8080/logout\">Log out</a>\n" +
+                        "    </p>\n" +
+                        "  </body>\n" +
+                        "</html>\n";
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Only users can see this";
+    }
+
+    @GetMapping("/admins")
+    public String getAdmins() {
+        return "Only admins can see this";
+    }
+
+
+}
---
+++ src/main/java/com/spring/lessons/springlessons/security/WebSecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.spring.lessons.springlessons.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        requests ->
+                                requests
+                                        .antMatchers("/users")
+                                        .hasRole("USER")
+                                        .antMatchers("/admins")
+                                        .hasRole("ADMIN")
+                                        .anyRequest()
+                                        .authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}password")
+                //.password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                //.password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
